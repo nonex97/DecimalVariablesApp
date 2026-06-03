b445fef482a407cf33cab94863efa03c0306e379
nonex97 <ngrguric97@gmail.com>
2026-06-03 16:07:58 +0200

    Learned what a decimal is, how to properly assign it, and when it is used.

--- DecimalVariables/Program.cs
+++ DecimalVariables/Program.cs
@@ -1,4 +1,14 @@
 ﻿
 
+// decimal is similar to a double, but it is more precise. stores values up to 28 digits long. so they are used where precision is important (banks, spaceflight, ...)
 
 
+// Int32 - 000000000 000000000 000000000 000000001 (1 as an int)
+
+
+decimal bankAccountBalance;
+
+//bankAccountBalance = 2.34; // this is a double
+bankAccountBalance = 2.34M; // this is a decimal, the M is only needed when you are assigning it a value. if it's retrieved from a database it's not needed
+
+Console.WriteLine(bankAccountBalance);
